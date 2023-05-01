f27006211a03a5343e6bcfd76ff8c848a6b695be
t-benedek <t.benedek@web.de>
2023-05-01 17:29:28 +0200

    removed details election fragment as it is replaced by voter info fragment

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionDetailsFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionDetailsFragment.kt
@@ -10,25 +10,7 @@ import androidx.lifecycle.lifecycleScope
 import com.example.android.politicalpreparedness.App
 import com.example.android.politicalpreparedness.database.ElectionDatabase
 import com.example.android.politicalpreparedness.database.ElectionRepo
-import com.example.android.politicalpreparedness.databinding.FragmentElectionDetailsBinding
 import com.example.android.politicalpreparedness.network.models.Election
 import kotlinx.coroutines.*
 
 private const val LOGTAG = "ElectionDetail"
-class ElectionDetailsFragment : Fragment() {
-
-    private val electionDao = ElectionDatabase.getInstance(App.context).electionDao
-    private val repo = ElectionRepo(electionDao)
-
-        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                                  savedInstanceState: Bundle?): View? {
-            val binding = FragmentElectionDetailsBinding.inflate(inflater)
-            binding.lifecycleOwner = this
-
-            return binding.root
-        }
-
-
-
-
-}
